9ebb300609f3dbd44981b50fe0686499dfef38cb
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-02-22 17:16:55 +0300

    Initial Commit

---
+++ WomenInScience/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WomenInScience
+//
+//  Created by Derya Çayıroğlu on 22.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
