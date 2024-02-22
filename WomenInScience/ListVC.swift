5f8fa4dd3acf2e01cf6b61ac1c152bde1a5b0409
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-02-22 18:13:38 +0300

    ListVC added

---
+++ WomenInScience/ListVC.swift
@@ -0,0 +1,17 @@
+//
+//  ListVC.swift
+//  WomenInScience
+//
+//  Created by Derya Çayıroğlu on 22.02.2024.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
