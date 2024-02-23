c40ab7788ba06da7386ef6b43cea876da438ae57
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-02-23 18:12:53 +0300

    Women class created

---
+++ WomenInScience/Model/Women.swift
@@ -0,0 +1,18 @@
+//
+//  Women.swift
+//  WomenInScience
+//
+//  Created by Derya Çayıroğlu on 23.02.2024.
+//
+
+import Foundation
+
+class Women: Codable {
+    var Adi: String
+    var Alani: String
+    var DogumTarihi: String
+    var OlumTarihi: String
+    var KapakFotoUrl: String
+    var LogoUrl: String
+    var Aciklama: String
+}
--- WomenInScience/Screens/ListVC.swift
+++ WomenInScience/Screens/ListVC.swift
@@ -12,6 +12,7 @@ class ListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        navigationController?.isNavigationBarHidden = true
     }
 
 }
