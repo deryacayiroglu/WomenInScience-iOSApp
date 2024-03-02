d22f240fe222159cae7554948321a19edb0574b3
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-03-02 17:33:27 +0300

    DetailVC created

---
+++ WomenInScience/Screens/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  WomenInScience
+//
+//  Created by Derya Çayıroğlu on 2.03.2024.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
