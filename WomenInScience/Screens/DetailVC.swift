2a33d5e7f514f9639588ef8fb5e4aeecf9e87823
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-03-02 18:24:55 +0300

    background image added on detail

--- WomenInScience/Screens/DetailVC.swift
+++ WomenInScience/Screens/DetailVC.swift
@@ -13,8 +13,21 @@ class DetailVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setBackgroundImage()
         print(woman.Adi)
+    }
+    
+    func setBackgroundImage() {
+        let background = UIImage(named: "DetailBackground")
+
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
     }
 
 }
