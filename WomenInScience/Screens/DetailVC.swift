5a02d145592a96699287a56385bf000a681dae03
Derya Cayiroglu <deryacayroglu@gmail.com>
2024-03-03 22:25:03 +0300

    logo imageView added detail screen

--- WomenInScience/Screens/DetailVC.swift
+++ WomenInScience/Screens/DetailVC.swift
@@ -10,10 +10,13 @@ import UIKit
 class DetailVC: UIViewController {
     
     var woman: Woman!
+    
+    let logoImageView = LogoImageView(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackgroundImage()
+        configureLogoImageView()
         print(woman.Adi)
     }
     
@@ -28,6 +31,22 @@ class DetailVC: UIViewController {
         imageView.center = view.center
         view.addSubview(imageView)
         self.view.sendSubviewToBack(imageView)
+    }
+    
+    func configureLogoImageView() {
+        logoImageView.downloadImage(from: woman.LogoUrl)
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        logoImageView.layer.borderWidth = 1
+        logoImageView.layer.borderColor = UIColor.systemGray.cgColor
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor, multiplier: 1.5)
+        ])
     }
 
 }
